c331c103bbb1e59cce5cf6a343e0cfb092ad2bb9
Diran Li <diranl@palantir.com>
2017-08-01 11:33:35 -0400

    First card displaying classic wu-tang quote

--- hiphopevents-app/App.js
+++ hiphopevents-app/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, Card } from 'react-native-material-design';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Card>
+          <Card.Body>
+            <Text>Wu Tang Clan ain't notin to fuck wit</Text>
+          </Card.Body>
+        </Card>
+        <Button text="Click Me"/>
       </View>
     );
   }
