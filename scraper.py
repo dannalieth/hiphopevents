13de1fdf83950331160b4e0c6dfe5ba5b6b83d37
Diran Li <diranl@palantir.com>
2017-08-01 15:25:34 -0400

    Scraper: scrapes the events from nuyorican into json file

--- scraper.py
+++ scraper.py
@@ -1,11 +1,48 @@
 from bs4 import BeautifulSoup as Soup
 import requests
+import json
 
-response = requests.get('http://www.nuyorican.org/calendar/')
-soup = Soup(response.content, "lxml")
-selections = soup.select('td.has-event')
 
-event = selections[0]
+class Scraper:
+    def __init__(self):
+        pass
 
-date = event.select_one('.date .value-title').get('title')
-print('selections:', selections)
+    @staticmethod
+    def run():
+        html = Scraper._rawHtml()
+        eventsJson = Scraper._daysJsonFromHtml(html)
+        Scraper._writeJsonToFile(eventsJson)
+
+    @staticmethod
+    def _rawHtml():
+        response = requests.get('http://www.nuyorican.org/calendar/')
+        return Soup(response.content, "lxml")
+
+    @staticmethod
+    def _daysJsonFromHtml(soup: Soup):
+        return [Scraper._dayJsonFromDayDom(day) for day in soup.select('td.has-event') if len(day.select('.one-event')) > 0]
+
+    @staticmethod
+    def _dayJsonFromDayDom(day: Soup):
+        return {
+            "date": day.select_one('.date .value-title').get('title'),
+            "events": [Scraper._eventJsonFromEventDom(event) for event in day.select('.one-event')],
+        }
+
+    @staticmethod
+    def _eventJsonFromEventDom(event: Soup):
+        return {
+            "eventLink": "http://www.nuyorican.org{0}".format(event.select_one('a').get('href')),
+            "image": event.select_one('a > img').get('src'),
+            "title": event.select_one('.headliners a').get_text(),
+            "startTime": event.select_one('.start-time').get_text(),
+            "ticketLink": event.select_one('.ticket-link a').get('href'),
+        }
+
+    @staticmethod
+    def _writeJsonToFile(eventsJson):
+        with open('nuyorican.json', 'w') as f:
+            f.write(json.dumps(eventsJson, indent=2))
+
+if __name__ == '__main__':
+    Scraper.run()
