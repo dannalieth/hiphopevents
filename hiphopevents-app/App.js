f5740b876b3d2d22592267c44c0176856a87327c
Diran Li <diranl@palantir.com>
2017-08-01 12:02:03 -0400

    Got some fetching action

--- hiphopevents-app/App.js
+++ hiphopevents-app/App.js
@@ -1,27 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
 import { Button, Card } from 'react-native-material-design';
 
 export default class App extends React.Component {
+
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    this._scrapeEvents();
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+          <View style={{flex: 1, paddingTop: 20}}>
+            <ActivityIndicator />
+          </View>
+        );
+    }
+
     return (
       <View style={styles.container}>
         <Card>
           <Card.Body>
-            <Text>Wu Tang Clan ain't notin to fuck wit</Text>
+            <Text>Wu Tang Clan ain't notin to fuck wit. asdf</Text>
           </Card.Body>
+          <Card.Actions>
+            <Button text="Enter the Thirty-Six Chambers"/>
+          </Card.Actions>
         </Card>
-        <Button text="Click Me"/>
       </View>
     );
+  }
+
+  _scrapeEvents = () => {
+    return fetch('http://www.nuyorican.org/calendar/')
+      .then((response) => {
+        console.log(response);
+        this.setState({ isLoading: false });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
